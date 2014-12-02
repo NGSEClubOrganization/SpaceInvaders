082e823947853c60a24d689b02456fdbbd64ed54
jaim675 <jaim675@nhs-65-13.nhs.local>
2014-12-02 09:06:03 -0800

    Upgrade powerup box error fix (x-l   =>   x-(l/2))

--- SpaceInvaders/src/com/ngse/spaceinvaders/gameobjects/Upgrade.java
+++ SpaceInvaders/src/com/ngse/spaceinvaders/gameobjects/Upgrade.java
@@ -10,7 +10,7 @@ import com.ngse.spaceinvaders.Config;
 public class Upgrade extends GameObject {
 	
 	private double speed;
-	private int l; //l = length
+	private double l; //l = length
 	private Random rand = new Random();
 	
 	public Upgrade(double x, double y, double dx, double dy, double speed,
@@ -51,15 +51,15 @@ public class Upgrade extends GameObject {
 	
 	public Rectangle2D.Double getBox(){
         l = 5;
-        return new Rectangle2D.Double(x-l, y-l, l, l);
+        return new Rectangle2D.Double(x-(l/2), y-(l/2), l, l);
     }
     
     public void checkBoundary(){
-        if (    (x-l > 0 && x+l < Config.FRAME_WIDTH) == false  ){
+        if (    (x-(l/2) > 0 && x+(l/2) < Config.FRAME_WIDTH) == false  ){
             dx *= -1;
         }
         
-        if (    (y-l > 0 && y+l < Config.FRAME_HEIGHT) == false  ){
+        if (    (y-(l/2) > 0 && y+(l/2) < Config.FRAME_HEIGHT) == false  ){
             dy *= -1;
         }
     }
