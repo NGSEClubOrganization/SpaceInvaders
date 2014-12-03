16578eccd8827f952cf45e1eb34b9e9152820729
AlphaBravo314 <alpha.blumer@gmail.com>
2014-12-02 17:55:13 -0800

    Update to home pc

---
+++ SpaceInvaders/src/com/ngse/spaceinvaders/ai/AlienBossAI.java
@@ -0,0 +1,9 @@
+package com.ngse.spaceinvaders.ai;
+
+public class AlienBossAI {
+
+	public void doLogic() {
+		// TODO all logic from privates below (main method kinda)
+	}
+
+}
---
+++ SpaceInvaders/src/com/ngse/spaceinvaders/ai/AlienSystemAI.java
@@ -0,0 +1,13 @@
+package com.ngse.spaceinvaders.ai;
+
+public class AlienSystemAI {
+
+	public enum AlienPattern {
+		
+	}
+	
+	public void doLogic() {
+		// TODO all logic from privates below (main method kinda)
+	}
+
+}
